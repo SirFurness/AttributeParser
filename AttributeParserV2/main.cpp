c84f29c6f68820d392e5f48b18e99f133b148a4d
furnessBoss <eric.1334@icloud.com>
2016-02-25 23:47:46 -0800

    Initial Commit

---
+++ AttributeParserV2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AttributeParserV2
+//
+//  Created by Eric Paul on 2/25/16.
+//  Copyright © 2016 Eric Paul. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
